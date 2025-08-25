199bd38b67631adace53a5e560151c130dc7fee1
Paul Isaris <paulisaris@gmail.com>
2025-08-25 11:50:17 +0300

    pestphp

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,5 +9,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase {
     protected function setUp(): void {
         parent::setUp();
+        // Clear Laravel caches before each test suite
+        \Artisan::call('route:clear');
+        \Artisan::call('config:clear');
+        \Artisan::call('cache:clear');
+        \Artisan::call('view:clear');
     }
 }
